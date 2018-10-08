8060d3b7361657f1f6f2f70dd74a810948987c20
Markos-Vasalos <markos.vasalos1@gmail.com>
2018-10-08 23:16:27 +0300

    Add files via upload

---
+++ python_files/open_the_light.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO  
+# to use Raspberry Pi board pin numbers  
+GPIO.setmode(GPIO.BOARD)  
+# set up GPIO output channel  
+GPIO.setup(11, GPIO.OUT)  
+# open the relay that controls the device
+GPIO.output(11,GPIO.HIGH)
